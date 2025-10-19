4719f668b22895492f8224339f3796de7b3af16c
Eduardo Tachotte <bfwaend@gmail.com>
2025-10-19 17:02:45 -0300

    Properly parse flags

--- spider/pkg/options/options.go
+++ spider/pkg/options/options.go
@@ -2,12 +2,15 @@ package options
 
 import (
 	"flag"
+	"fmt"
+	"os"
 )
 
 type Options struct {
 	UseRecursion   bool
 	RecursionDepth int
 	DataPath       string
+	Arguments	   []string
 }
 
 func NewOptions() *Options {
@@ -15,11 +18,22 @@ func NewOptions() *Options {
 	flagRecursionDepth := flag.Int("l", 5, "Maximum depth for recursion")
 	flagDataPath := flag.String("p", "./data", "Path to store downloaded files")
 
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage: spider [-rlp] URL\n\nFlags:\n")
+		flag.PrintDefaults()
+	}
+
 	flag.Parse()
+
+	if flag.NArg() != 1 {
+		flag.Usage()
+		os.Exit(1)
+	}
 
 	return &Options {
 		*flagUseRecursion,
 		*flagRecursionDepth,
 		*flagDataPath,
+		flag.Args(),
 	}
 }
